5bad863d655ce5995e43492b91b343abdcb2886f
김보석 <boseoggim08@gmail.com>
2021-11-12 00:20:03 +0900

    secret_key issue complete

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1+wn$^6#48k&&hxg!1d=giahxlpe(z%c*68_#&@4i2c%7vpgs9'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
