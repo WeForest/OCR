3de18eb1acf66bd2cf6c11541f657ecb6b3e119f
김보석 <boseoggim08@gmail.com>
2021-11-18 11:25:36 +0900

    host 설정

--- config/settings.py
+++ config/settings.py
@@ -90,6 +90,11 @@ DATABASES = {
     }
 }
 
+ALLOWED_HOSTS = [
+    'ocrapiserver.herokuapp.com',
+    '127.0.0.1'
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
