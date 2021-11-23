141bd5511e880e675067dea96e10e9335af0fbb7
김보석 <boseoggim08@gmail.com>
2021-11-24 02:10:15 +0900

    이미지 받아부럿으

--- client_certification/serializers.py
+++ client_certification/serializers.py
@@ -1,7 +1,1 @@
 from rest_framework import serializers
-from client_certification.models import ImageUpload
-
-class ImageUploadSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = ImageUpload
-        fields = ('url', 'pk', 'title', 'imagefile')
