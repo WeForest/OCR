f1b1a9196c461ef9e23fc423e55ea8100259aae5
김보석 <boseoggim08@gmail.com>
2021-11-26 15:18:23 +0900

    django delete

--- client_certification/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class ImageUpload(models.Model):
-    title = models.CharField(max_length=100)
-    imagefile = models.FileField(null=True)
-    #imagefile = models.FileField(upload_to='imagefile/%Y/%m/%d', null=True)
